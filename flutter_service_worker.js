c3c82777542e93c22ba47031d5250bfa5a7c5855
Mo Alaklouk <61121384+MoAlaklouk@users.noreply.github.com>
2023-06-02 21:30:08 +0300

    pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "32101375ba74a26a16bf3007db2b7ad5",
-"/": "32101375ba74a26a16bf3007db2b7ad5",
-"main.dart.js": "972ff501d5ca5066e52c57d59d2e2e38",
+"index.html": "8c2071561cff9e4d31f8f9bbbee8f35d",
+"/": "8c2071561cff9e4d31f8f9bbbee8f35d",
+"main.dart.js": "6cdb8b0c606c7bfde52aeb581151caec",
 "manifest.json": "2fb9188af0bb263135877d3e3a776209",
 "version.json": "9094aacdae789dccd67fa32109ff1a18"
 };
